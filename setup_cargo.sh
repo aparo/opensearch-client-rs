9a3e09d0fd22fa0f2f9b90ca4d93ab0cb51cb483
alberto.paro <alberto.paro@gmail.com>
2023-09-24 18:39:48 +0200

    [OpenSearch] Initial import of project

---
+++ setup_cargo.sh
@@ -0,0 +1,7 @@
+rustup component add clippy rustfmt llvm-tools-preview wasm32-unknown-unknown
+cargo install cargo-watch
+cargo install cargo-machete
+cargo install sea-orm-cli
+cargo install cargo-sort
+cargo install cargo-xtask
+cargo install grcov
